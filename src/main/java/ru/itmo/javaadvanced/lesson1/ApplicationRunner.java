a7068895ca90fec52666a3828fe1292b5c46fcfe
vitr1988 <vitr@list.ru>
2025-09-04 22:34:04 +0400

    fix: replace constant to local variable

--- src/main/java/ru/itmo/javaadvanced/lesson1/ApplicationRunner.java
+++ src/main/java/ru/itmo/javaadvanced/lesson1/ApplicationRunner.java
@@ -7,11 +7,10 @@ import ru.itmo.javaadvanced.lesson1.dto.ExampleDto;
 @SpringBootApplication
 public class ApplicationRunner {
 
-	public static final ExampleDto EXAMPLE_DTO = new ExampleDto();
-
 	public static void main(String[] args) {
-		EXAMPLE_DTO.setId(1L);
-		System.out.println(EXAMPLE_DTO.getId());
+		ExampleDto exampleDto = new ExampleDto();
+		exampleDto.setId(1L);
+		System.out.println(exampleDto.getId());
 
 		SpringApplication.run(ApplicationRunner.class, args);
 	}
